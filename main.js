c6d2fd256cc3472ea9a6c640167ae877d323a014
barbara_workman <susan.workman90@gmail.com>
2020-06-08 13:48:55 -0400

    .

--- main.js
+++ main.js
@@ -10,19 +10,48 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // this array.
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+function getCellElements(currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
 
 // YOUR CODE GOES HERE
+replaceAllButton.addEventListener('click', function () {
+    console.log('replace-all-button, clicked');
+    let find = findInput.value
+    console.log(find)
+    let replace = replaceInput.value
+    console.log(replace)
+
+})
+
+let numbers = [10, 20, 30, 40, 50];
+for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index]);
+    index = 0;
+    array = [1, 2, 3, 4, 5, 6];
+    while (index < array.length) {
+        console.log(array[index]);
+        index++;
+    }
+}
+function cellFunc(x) {
+    let cell = document.getElementById(x).rows[0].cells;
+    document.write(cell[0].innerHTML);
+}
+
+let string = "Griffin";
+let n = str.includes("McClain");
+string.includes(searchvalue, start)
 
 
+const content = getCellElement.innerHTML;
+getCellElement.innerHTML = htmlString;
 // One last thing: dedicate very careful attention to using variables and
 // naming them accurately.
 // And when you change the value you are assigning to a variable, don't
